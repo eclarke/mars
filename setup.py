44ddc2ccac910a7e76619d9bed29b147677e7db5
Erik Clarke <ecl@pennmedicine.upenn.edu>
2019-03-04 17:39:35 -0500

    use scm version number

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
+#from distutils.core import setup
 
 setup(
     name="mars",
     setup_requires=['setuptools_scm'],
     packages=["mars"],
-    use_scm_version = {"root": ".", "relative_to": __file__},
+    use_scm_version = True,
     include_package_data=True,
     package_data={"mars": [
         "mars/data/config.schema.yaml",
